bceb4c4d4cec3e0fc520825fe6193ea78da7e7d3
valery <wogl@mail.ru>
2022-11-15 14:55:08 +0300

    area of a circle, площадь круга

---
+++ example1/Program.cs
@@ -0,0 +1,7 @@
+﻿// пишем программу для нахождения площади круга Pi*r^2
+const double pi = 3.14;
+double radius;
+Console.Write("Введите радиус круга: ");
+radius = Convert.ToDouble(Console.ReadLine());
+Console.Write("Площадь круга равна: ");
+Console.WriteLine(pi * (radius * radius));
