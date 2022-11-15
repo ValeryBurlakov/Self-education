82bff633ddf87ecc83e405b23613ff7b32e6779b
valery <wogl@mail.ru>
2022-11-15 15:24:15 +0300

    other function, другая  функция

---
+++ example3/Program.cs
@@ -0,0 +1,7 @@
+﻿// функция x = 12a^2 + 7a + 12
+double a;
+Console.Write("Введите a: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("a = ");
+Console.WriteLine((12 *(a*a)) +(7 * a) + 12);
+
