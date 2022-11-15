85d4f383184a740a8b66055077f406aff362ae00
valery <wogl@mail.ru>
2022-11-15 15:10:35 +0300

    function, функция

---
+++ example2/Program.cs
@@ -0,0 +1,6 @@
+﻿// вычисление функции y = 7x^2 + 3x + 6
+double x;
+Console.Write("Введите X: ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write("y = ");
+Console.WriteLine((7*(x*x)) + (3 * x) + 6);
