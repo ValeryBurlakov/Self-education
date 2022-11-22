d759013a63052aa36019d571c82ead2acc404e35
Valery <wogl@mail.ru>
2022-11-22 19:31:34 +0300

    added example 8 9 10 11

---
+++ example10/Program.cs
@@ -0,0 +1,27 @@
+﻿//https://c-sharp.pro/a2-%d1%86%d0%b5%d0%bb%d0%be%d1%87%d0%b8%d1%81%d0%bb%d0%b5%d0%bd%d0%bd%d0%b0%d1%8f-%d0%b0%d1%80%d0%b8%d1%84%d0%bc%d0%b5%d1%82%d0%b8%d0%ba%d0%b0-%d0%b2%d1%8b%d0%b4%d0%b5%d0%bb%d0%b5%d0%bd%d0%b8%d0%b5/
+//A2.3. Дано трехзначное число. Найти:
+// а) число единиц в нем;
+// б) число десятков в нем;
+// в) сумму его цифр;
+// г) произведение его цифр.
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = number / 100;
+Console.WriteLine(number2);
+if (number2 > 0 && number2 < 10)
+{
+    Console.WriteLine($"Всё ок, ваше число: {number}");
+    int desiyatki = (number / 10) % 10;
+    Console.WriteLine($"число десятков: {desiyatki}");
+    int edinizi = number % 10;
+    Console.WriteLine($"число единиц: {edinizi}");
+    int sum = number2 + desiyatki + edinizi;
+    Console.WriteLine($"сумма цифр числа: {sum}");
+    int proizved = number2 * desiyatki * edinizi;
+    Console.WriteLine($"произведение цифр числа: {proizved}");
+}
+else
+{
+    Console.WriteLine("вы ввели не трёхзначное число");
+}
---
+++ example11/Program.cs
@@ -0,0 +1,50 @@
+﻿// A2.4. Дано трехзначное число. Найти число, полученное при прочтении его цифр справа налево.
+// A2.5. Дано трехзначное число. В нем зачеркнули первую слева цифру и приписали ее в конце. Найти полученное число.
+// A2.6. Дано трехзначное число. В нем зачеркнули последнюю справа цифру и приписали ее в начале. Найти полученное число.
+// A2.7. Дано трехзначное число. Найти число, полученное при перестановке первой и второй цифр заданного числа.
+// A2.8. Дано трехзначное число. Найти число, полученное при перестановке второй и третьей цифр заданного числа.
+// A2.9. Дано трехзначное число, в котором все цифры различны. Получить шесть чисел, образованных при перестановке цифр заданного числа.
+
+Console.WriteLine("Введите трехзначное число");
+int arr = Convert.ToString(Console.ReadLine());
+Console.WriteLine(num[0]);
+// int position = i;
+// for (int i = 0; i < num.Length; i++)
+//         int temporary = num[i]; // производится замена элемента
+//         num[i] = num[position];
+//         num[position] = temporary;
+// Console.Write(num);
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+    for (int j = i + 1; j < array.Length; j++)// ищет максимальный элемент
+    {
+        if (array[j] < array[minPosition]) 
+        {
+            minPosition = j;
+        }
+    }
+        int temporary = array[i]; // производится замена элемента
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
